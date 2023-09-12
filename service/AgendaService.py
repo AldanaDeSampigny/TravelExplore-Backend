376321060f99ba107bb2b2e7d980f641ee1ba795
Aldana <aldanadesampigny2017@hotmail.com>
2023-09-12 04:28:20 -0300

    feat: :sparkles: Intento de carga de agenda

--- service/AgendaService.py
+++ service/AgendaService.py
@@ -1,5 +1,8 @@
 from ..repository.MeGustaRepository import MeGustaRepository
 from ..models.MeGustas import MeGustas
+from ..models.Agendas import Agendas
+from ..models.DiaViaje import DiaViaje
+from ..models.Actividad import Actividad
 from ..repository.AgendaRepository import AgendaRepository
 from ..bd.conexion import getEngine
 from sqlalchemy.orm import Session
@@ -8,6 +11,31 @@ from datetime import datetime, timedelta
 class AgendaService:
     def __init__(self, db_session):
         self.db_session = db_session
+
+
+    def guardarAgendaEnBd(self, agenda):
+        with Session(getEngine()) as session:
+            agendaNueva = Agendas()
+            agendaNueva.id = 105
+            agendaNueva.horaInicio = "12:00:00"
+            agendaNueva.horaFin = "15:00:00"
+            agendaNueva.fechaDesde = "2023-05-15"
+            agendaNueva.fechaHasta = "2023-05-17"
+            agendaNueva.usuario_id = 1
+
+            try:
+                session.add(agendaNueva)
+                session.commit()
+            
+            except Exception as e:
+                # En caso de error, realiza un rollback
+                session.rollback()
+                raise e
+            
+            finally:
+                # Cierra la sesión
+                session.close()
+
 
     def generar_agenda(self, usuarioID, viajeID):
         with Session(getEngine()) as session:
@@ -37,25 +65,25 @@ class AgendaService:
 
                         if m.tipo == 'restaurant' and hora_inicio in horas and m.horarioApertura < hora_inicio < m.horarioCierre:
                             if m.id not in gustos_agregados:
-                                actividad_data = {
+                                actividad = {
                                     'dia': dia,
                                     'hora_inicio': hora_inicio,
                                     'hora_fin': hora_cierre_intervalo,
                                     'actividad': m
                                 }
-                                agenda.append(actividad_data)
+                                agenda.append(actividad)
                                 gustos_agregados.add(m.id)
                                 break
                         
                         if m.horarioApertura < hora_inicio < m.horarioCierre:
                             if m.id not in gustos_agregados:
-                                actividad_data = {
+                                actividad = {
                                     'dia': dia,
                                     'hora_inicio': hora_inicio,
                                     'hora_fin': hora_cierre_intervalo,
                                     'actividad': m
                                 }
-                                agenda.append(actividad_data)
+                                agenda.append(actividad)
                                 gustos_agregados.add(m.id)
                                 break
 
@@ -66,29 +94,6 @@ class AgendaService:
                     inicioNumerico = hora_inicio.hour * 60 + hora_inicio.minute
 
             return agenda
-
-    """ def horariosDias(self, fechaDesde, fechaHasta, horaDesde, horaHasta):
-        diasHorarios = {}
-
-        fechaDesdeDate = datetime.strptime(fechaDesde, "%Y-%m-%d")
-        fechaHastaDate = datetime.strptime(fechaHasta, "%Y-%m-%d")
-
-        fechaActual = fechaDesdeDate
-
-        # "11-9-22" al "20-9-22" desde las "9:00" hasta las "13:00"
-
-        while fechaActual <= fechaHastaDate:
-            # Obtiene el nombre del día de la semana
-            diaSemana = fechaActual.strftime("%A")
-
-            # Agrega el rango horario al diccionario
-            diasHorarios[diaSemana] = (horaDesde, horaHasta)
-
-            # Avanza al siguiente día
-            fechaActual += timedelta(days=1)
-
-        return diasHorarios
-    """
 
     def generarAgendaPersonalizada(self, usuarioID, viajeID, horariosElegidos):
         with Session(getEngine()) as session:
@@ -123,25 +128,25 @@ class AgendaService:
 
                         if m.tipo == 'restaurant' and hora_actual in horas and m.horarioApertura < hora_actual < m.horarioCierre:
                             if m.id not in gustos_agregados:
-                                actividad_data = {
+                                actividad = {
                                     'dia': dia,
                                     'hora_inicio': hora_actual,
                                     'hora_fin': hora_cierre_intervalo.time(),
                                     'actividad': m
                                 }
-                                agenda.append(actividad_data)
+                                agenda.append(actividad)
                                 gustos_agregados.add(m.id)
                                 break
 
                         if m.horarioApertura < hora_actual < m.horarioCierre:
                             if m.id not in gustos_agregados:
-                                actividad_data = {
+                                actividad = {
                                     'dia': dia,
                                     'hora_inicio': hora_actual,
                                     'hora_fin': hora_cierre_intervalo.time(),
                                     'actividad': m
                                 }
-                                agenda.append(actividad_data)
+                                agenda.append(actividad)
                                 gustos_agregados.add(m.id)
                                 break
                     if datetime.strptime('00:00:00', '%H:%M:%S').time() <= hora_actual <= datetime.strptime('04:00:00', '%H:%M:%S').time():
@@ -149,47 +154,6 @@ class AgendaService:
 
                     hora_actual = (hora_cierre_intervalo + timedelta(minutes=30)).time()
         return agenda
-
-
-    """  def buscarViaje(self, usuario_id):
-        session = Session(self.db_session)
-
-        result = session.query(Viajes)\
-                .join(Usuarios)\
-                .filter(Viajes.usuario_id == usuario_id)\
-                .all()
-                
-        return result """
-    
-    """def generarAgendaPersonalizada(self, usuarioID, horariosElegidos):
-        with Session(getEngine()) as session:
-            print(usuarioID, horariosElegidos, "service")
-            agenda_repo = AgendaRepository(session)
-            meGustas_ids = agenda_repo.buscarGustosPersonalizado(usuarioID, self.buscarViaje(usuarioID))
-            agenda = {}
-
-            for dia, (hora_inicio, hora_fin) in horariosElegidos.items():
-                hora = hora_inicio
-                agenda[dia] = {}  # Inicializa el diccionario del día
-
-                while hora < hora_fin:
-                    for m_id in meGustas_ids:
-                        m = session.query(MeGustas).get(m_id[0])
-
-                        if m.tipo == 'restaurant':
-                            if (hora, hora + m.duracion) not in agenda[dia]:
-                                agenda[dia][(hora, hora + m.duracion)] = m.id
-
-                        if m.horaApertura < hora < m.horaCierre:
-                            if (hora, hora + m.duracion) not in agenda[dia]:
-                                agenda[dia][(hora, hora + m.duracion)] = m.id
-
-                        hora += m.duracion + 0.5
-
-                    dia += 1
-
-        return agenda """
-        
 
 
     #viaje = buscarViaje(usuarioID=123)
@@ -241,49 +205,49 @@ class AgendaService:
                             else:
                                 if m.tipo == 'restaurant' and hora_inicio in horas and m.horarioApertura < hora_inicio < m.horarioCierre:
                                     if m.id not in gustos_agregados:
-                                        actividad_data = {
+                                        actividad = {
                                             'dia': dia,
                                             'hora_inicio': hora_inicio,
                                             'hora_fin': hora_cierre_intervalo,
                                             'actividad': m
                                         }
-                                        agenda.append(actividad_data)
+                                        agenda.append(actividad)
                                         gustos_agregados.add(m.id)
                                         break
                                     
                                 if m.horarioApertura < hora_inicio < m.horarioCierre:
                                     if m.id not in gustos_agregados:
-                                        actividad_data = {
+                                        actividad = {
                                             'dia': dia,
                                             'hora_inicio': hora_inicio,
                                             'hora_fin': hora_cierre_intervalo,
                                             'actividad': m
                                         }
-                                        agenda.append(actividad_data)
+                                        agenda.append(actividad)
                                         gustos_agregados.add(m.id)
                                         break
                         else:
                             if m.tipo == 'restaurant' and hora_inicio in horas and m.horarioApertura < hora_inicio < m.horarioCierre:
                                 if m.id not in gustos_agregados:
-                                    actividad_data = {
+                                    actividad = {
                                         'dia': dia,
                                         'hora_inicio': hora_inicio,
                                         'hora_fin': hora_cierre_intervalo,
                                         'actividad': m
                                     }
-                                    agenda.append(actividad_data)
+                                    agenda.append(actividad)
                                     gustos_agregados.add(m.id)
                                     break
                                 
                             if m.horarioApertura < hora_inicio < m.horarioCierre:
                                 if m.id not in gustos_agregados:
-                                    actividad_data = {
+                                    actividad = {
                                         'dia': dia,
                                         'hora_inicio': hora_inicio,
                                         'hora_fin': hora_cierre_intervalo,
                                         'actividad': m
                                     }
-                                    agenda.append(actividad_data)
+                                    agenda.append(actividad)
                                     gustos_agregados.add(m.id)
                                     break
 
@@ -292,5 +256,9 @@ class AgendaService:
 
                     hora_inicio = (hora_cierre_intervalo + timedelta(minutes=30)).time()
                     inicioNumerico = hora_inicio.hour * 60 + hora_inicio.minute
-
+   
             return agenda
+
+   
+
+    
