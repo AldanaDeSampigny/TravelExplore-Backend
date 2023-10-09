c20e01891bc840dfd00a23c9bfec674bd49c71a1
Aldana <aldanadesampigny2017@hotmail.com>
2023-10-08 22:02:26 -0300

    feat: :sparkles: Prototipo IA y entrenamiento

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -7,6 +7,7 @@ from .bd.conexion import getSession, getEngine
 from .repository.UsuarioRepository import UsuarioRepository
 from .repository.CategoriaRepository import CategoriaRepository
 import tensorflow as tf
+import tensorflow_recommenders as tfrs
 import numpy as np
 
 engine = getEngine()
@@ -58,68 +59,155 @@ with Session(getEngine()) as session:
             else:
                 preferencias_usuarios[i, c] = 0
 
-    actividades_categorias = np.zeros((num_actividades, num_categorias))
-    claves_actividades = list(datosActividad.keys())
+    # actividades_categorias = np.zeros((num_actividades, num_categorias))
+    # claves_actividades = list(datosActividad.keys())
 
-    for i, actividad in enumerate(claves_actividades):
-        for j in range(0, num_categorias):
-            if j in datosActividad[actividad]:  # Ajusta esto según la estructura de tu modelo de datos
-                actividades_categorias[i, j] = 1
-            else:
-                actividades_categorias[i, j] = 0
+    # for i, actividad in enumerate(claves_actividades):
+    #     for j in range(0, num_categorias):
+    #         if j in datosActividad[actividad]:  # Ajusta esto según la estructura de tu modelo de datos
+    #             actividades_categorias[i, j] = 1
+    #         else:
+    #             actividades_categorias[i, j] = 0
 
     # Crear matrices NumPy para las actividades y preferencias
-    print("tamaño: actividades ", len(actividades_categorias))
+    print("tamaño: actividades ", len(actividades))
     print("tamaño: preferencia ", len(preferencias_usuarios))
 
-    if len(actividades_categorias) > len(preferencias_usuarios):
-        max_size = len(actividades_categorias)
+    # if len(actividades_categorias) > len(preferencias_usuarios):
+    #     max_size = len(actividades_categorias)
+    #     preferencias_usuarios = np.pad(preferencias_usuarios, ((0, max_size - len(preferencias_usuarios)), (0, 0)), 'constant')
+    # else:
+    #     max_size = len(preferencias_usuarios)
+    #     actividades_categorias = np.pad(actividades_categorias, ((0, max_size - len(actividades_categorias)), (0, 0)), 'constant')
+    if len(actividades) > len(preferencias_usuarios):
+        max_size = len(actividades)
         preferencias_usuarios = np.pad(preferencias_usuarios, ((0, max_size - len(preferencias_usuarios)), (0, 0)), 'constant')
     else:
         max_size = len(preferencias_usuarios)
-        actividades_categorias = np.pad(actividades_categorias, ((0, max_size - len(actividades_categorias)), (0, 0)), 'constant')
+        actividades = np.pad(actividades, ((0, max_size - len(actividades)), (0, 0)), 'constant')
     
     preferencias_usuarios = np.array(preferencias_usuarios)
-    actividades_categorias = np.array(actividades_categorias)
+    actividades = np.array(actividades)
 
-    # Crear un modelo de recomendación con TensorFlow
-    modelo = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=(num_categorias,)), #num_categorias
-        tf.keras.layers.Dense(16, activation='relu'),
-        tf.keras.layers.Dense(num_categorias, activation='sigmoid') #se puede cambiar por num_categorias?
+    users_dataset = tf.data.Dataset.from_tensor_slices(preferencias_usuarios)
+    activities_dataset = tf.data.Dataset.from_tensor_slices(actividades)
+
+    interactions_dataset = tf.data.Dataset.zip((users_dataset, activities_dataset))
+
+    train_size = int(0.8 * len(interactions_dataset))
+    train_dataset = interactions_dataset.take(train_size)
+    test_dataset = interactions_dataset.skip(train_size)
+
+    #desde aca lo nuevo
+    # Define el modelo de usuario (reemplaza con tu lógica)
+    user_model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(num_categorias,)),  # Input con la misma forma que las preferencias de usuarios
+    tf.keras.layers.Dense(32, activation='relu'),
+    # Agrega más capas si es necesario
     ])
-    #modelo.add(tf.keras.layers.Dense(4, activation='sigmoid')) 
 
-    # Compilar el modelo
-    modelo.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# Define el modelo de actividad (reemplaza con tu lógica)
+    activity_model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(num_categorias,)),  # Input con la misma forma que las actividades
+        tf.keras.layers.Dense(32, activation='relu'),
+    # Agrega más capas si es necesario
+    ])
 
-    # Entrenar el modelo
-    #!volver a 1000
-    modelo.fit(preferencias_usuarios, actividades_categorias, epochs = 500)
+#Define tu tarea de recuperación (puede necesitar ajustes según tus preferencias)
+    task = tfrs.tasks.Retrieval(metrics=tfrs.metrics.FactorizedTopK(
+        activities.batch(128).map(activity_model)
+    ))
 
-    """nuevoUsuario = np.array([[0, 0, 0, 0, 1, 0, 0, 0]])"""
+# Define tu modelo de recomendación
+    class MyModel(tfrs.Model):
+        def __init__(self, user_model, activity_model, task):
+            super().__init__()
+            self.activity_model: tf.keras.Model = activity_model
+            self.user_model: tf.keras.Model = user_model
+            self.task: tf.keras.layers.Layer = task
+
+    def compute_loss(self, features, training=False):
+        user_embeddings = self.user_model(features["user_id"])
+        activity_embeddings = self.activity_model(features["activity_id"])
+        return self.task(user_embeddings, activity_embeddings)
+
+    # Crea una instancia de tu modelo
+    model = MyModel(user_model, activity_model, task)
+
+    # Compila el modelo
+    model.compile(optimizer=tf.keras.optimizers.Adagrad(0.5))
+
+    # Entrena el modelo (reemplaza con tus datos y número de épocas)
+    model.fit(preferencias_usuarios, actividades, epochs=50)
+
     usuario = CRepo.getCategoriaUsuario(5) 
     new = np.zeros(num_categorias, dtype=int) 
     for categoria in enumerate(usuario):
         if categoria in range(0, num_categorias):  # Asegúrate de que la categoría sea válida
             new[categoria] = 1
 
-    recomendaciones_probabilidades = modelo.predict(np.array([new]))#usuario_nuevo)
+    recomendaciones_probabilidades = model.predict(np.array([new]))#usuario_nuevo)
 
     # Obtener las actividades recomendadas en palabras
     print("recom: ", recomendaciones_probabilidades)
     actividades_recomendadas = []
     for i, probabilidad in enumerate(recomendaciones_probabilidades[0]):
         print("indice", i)
-        if probabilidad > 0.7:  # Puedes ajustar este umbral según tus preferencias
-            actividad = aRepo.getActividad(i+11)
-            print("actividad",actividad.nombre)
-            actividades_recomendadas.append(actividad.nombre)
+        #if probabilidad > 0.7:  # Puedes ajustar este umbral según tus preferencias
+        actividad = aRepo.getActividad(i+11)
+        print("actividad",actividad.nombre)
+        actividades_recomendadas.append(actividad.nombre)
 
     # Imprimir las recomendaciones de actividades
     print("Recomendaciones de actividades para el usuario:", actividades_recomendadas)
 
     # Guardar el modelo entrenado se guarda en h5 porque es un archivo compatible con tensorflow
-    modelo.save("modelo.keras")
+    model.save("modelo.keras")
 
     modelo_cargado = tf.keras.models.load_model("modelo.keras")
+
+    #aca empieza lo viejo
+    #actividades_categorias = np.array(actividades_categorias)
+
+    # Crear un modelo de recomendación con TensorFlow
+    # modelo = tf.keras.Sequential([
+    #     tf.keras.layers.Input(shape=(num_actividades,)), #num_categorias
+    #     tf.keras.layers.Dense(16, activation='relu'),
+    #     tf.keras.layers.Dense(num_actividades,activation='sigmoid') #se puede cambiar por num_categorias?
+    # ])
+    # #modelo.add(tf.keras.layers.Dense(4, activation='sigmoid')) 
+
+    # # Compilar el modelo
+    # modelo.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # # Entrenar el modelo
+    # #!volver a 1000
+    # objetivo_entrenamiento = preferencias_usuarios.copy()
+    # modelo.fit(preferencias_usuarios, objetivo_entrenamiento, epochs = 50)
+
+    # """nuevoUsuario = np.array([[0, 0, 0, 0, 1, 0, 0, 0]])"""
+    # usuario = CRepo.getCategoriaUsuario(5) 
+    # new = np.zeros(num_categorias, dtype=int) 
+    # for categoria in enumerate(usuario):
+    #     if categoria in range(0, num_categorias):  # Asegúrate de que la categoría sea válida
+    #         new[categoria] = 1
+
+    # recomendaciones_probabilidades = modelo.predict(np.array([new]))#usuario_nuevo)
+
+    # # Obtener las actividades recomendadas en palabras
+    # print("recom: ", recomendaciones_probabilidades)
+    # actividades_recomendadas = []
+    # for i, probabilidad in enumerate(recomendaciones_probabilidades[0]):
+    #     print("indice", i)
+    #     #if probabilidad > 0.7:  # Puedes ajustar este umbral según tus preferencias
+    #     actividad = aRepo.getActividad(i+11)
+    #     print("actividad",actividad.nombre)
+    #     actividades_recomendadas.append(actividad.nombre)
+
+    # # Imprimir las recomendaciones de actividades
+    # print("Recomendaciones de actividades para el usuario:", actividades_recomendadas)
+
+    # # Guardar el modelo entrenado se guarda en h5 porque es un archivo compatible con tensorflow
+    # modelo.save("modelo.keras")
+
+    # modelo_cargado = tf.keras.models.load_model("modelo.keras")
---
+++ prototipo.py
@@ -0,0 +1,79 @@
+import numpy as np
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Usuario, Viaje, Actividad, Lugar, AgendaDiaria
+from .bd.conexion import getSession, getEngine
+from .repository.UsuarioRepository import UsuarioRepository
+import numpy as np
+import tensorflow_recommenders as tfrs
+from typing import Dict, Text
+
+import numpy as np
+import tensorflow as tf
+from typing import Dict, Text
+
+
+# Crear mapas de palabras a números
+categorias_map = {
+    "Comida Italiana": 0,
+    "Deportes Acuáticos": 1,
+    "Senderismo": 2,
+    "Museos de Arte": 3,
+    "Fotografía de Naturaleza": 4
+}
+
+actividades_map = {
+    0: "Almuerzo en Trattoria Italiana",
+    1: "Paseo en Bote por el Lago",
+    2: "Escalada en Montaña"
+    # Agrega más actividades según sea necesario
+}
+
+usuarios_map = { #tiene las actividades que le gusta
+    "Usuario 1": [0, 1, 0, 0, 1],
+    "Usuario 2": [0, 0, 0, 1, 0],
+    "Usuario 3": [0, 0, 0, 0, 0]
+    # Agrega más usuarios según sea necesario
+}
+
+# Reemplazar datos numéricos por palabras
+categorias = np.array([
+    [1, 0, 0, 0, 0],  # Categoría 1: Comida Italiana
+    [0, 1, 1, 0, 0],  # Categoría 2: Deportes Acuáticos y Senderismo
+    [0, 0, 0, 1, 1]   # Categoría 3: Museos de Arte y Fotografía de Naturaleza
+])
+
+actividades = np.array([
+    [1, 0, 0],  # Almuerzo en Trattoria Italiana
+    [0, 1, 0],  # Paseo en Bote por el Lago
+    [0, 0, 1]   # Escalada en Montaña
+])
+
+# Crear un modelo de recomendación con TensorFlow
+modelo = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(categorias.shape[1],)),
+    tf.keras.layers.Dense(16, activation='relu'),
+    tf.keras.layers.Dense(actividades.shape[1], activation='sigmoid')
+])
+
+# Compilar el modelo
+modelo.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Entrenar el modelo
+modelo.fit(usuarios_map, actividades, epochs=1000)
+#"Usuario 3": [0, 0, 0, 0, 0]
+# Realizar predicciones para un usuario
+usuario_nuevo = np.array([0, 0, 1, 0, 1])#usuarios_map["Usuario 1"])
+recomendaciones_probabilidades = modelo.predict(np.array([usuario_nuevo]))
+
+# Obtener las actividades recomendadas en palabras
+actividades_recomendadas = []
+for i, probabilidad in enumerate(recomendaciones_probabilidades[0]):
+    if probabilidad > 0.5:  # Puedes ajustar este umbral según tus preferencias
+        actividades_recomendadas.append(actividades_map[i])
+
+# Imprimir las recomendaciones de actividades
+print("Recomendaciones de actividades para el usuario:", actividades_recomendadas)
+
+# Guardar el modelo entrenado se guarda en h5 porque es un archivo compatible con tensorflow
+modelo.save('modelo_entrenado.h5')
